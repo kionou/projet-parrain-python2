7736ba9b1fc498f011e4039eda1199e45db1082b
kionou <kionoumamadou.00@gmail.com>
2022-07-22 09:12:29 +0000

    fin

--- app.js
+++ app.js
@@ -9,28 +9,26 @@ mediaButton = document.getElementById("mediaButton"),
 navbar = document.querySelector('.nav');
 
 mediaButton.onclick = function () {
-    "use strict";
+ 
     mainListDiv.style.transition = "all 0.8s ease-in-out";
     mainListDiv.classList.toggle("show_list");
     mediaButton.classList.toggle("active");
 };
 
 
-// let icon = document.querySelector(".search");
-// let input = document.querySelector(" .inputsearch");
+let icon = document.querySelector(".search");
+let input = document.querySelector(" .inputsearch");
 
-// icon.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   input.classList.toggle("shownav_list");
-//   // icon.classList.toggle("active");
-  
-//   // input.style.display = "block";
+icon.addEventListener("click", (e) => {
+  e.preventDefault();
+   if (input.style.display === "none") {
+    input.style.display = "block";
+  } else {
+    input.style.display = "none";
+  }
 
-// });
 
-function myfonction (){
-  let input = document.querySelector(" .search-box input");
-  input.classList.toggle("shownav_list");
+});
 
-}
+
 
